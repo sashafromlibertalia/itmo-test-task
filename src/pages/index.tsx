9b606a7edfce2a8f742f03e4eba3028855a91a9f
Alexander <sashafromlibertalia@gmail.com>
2022-03-04 21:48:23 +0300

    refactor(code-style): configured ESLint

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,18 @@
-import type { NextPage } from 'next'
+import type {NextPage} from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../../styles/modules/Main.module.css'
 
 const Home: NextPage = () => {
-  return (
-    <div className={styles.container}>
-      <Head>
-        <title>ITMO test task</title>
-        <meta name="description" content="Built with love by @sashafromlibertalia" />
-      </Head>
-      Hello
-    </div>
-  )
+    return (
+        <div className={styles.container}>
+            <Head>
+                <title>ITMO test task</title>
+                <meta name="description" content="Built with love by @sashafromlibertalia"/>
+            </Head>
+            Hello
+        </div>
+    )
 }
 
 export default Home
