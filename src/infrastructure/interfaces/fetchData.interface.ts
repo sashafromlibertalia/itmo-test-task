1490718c3f464d36b239758a4e3392b112d74e03
Alexander <sashafromlibertalia@gmail.com>
2022-03-05 13:49:53 +0300

    fix(API): remove `void`

--- src/infrastructure/interfaces/fetchData.interface.ts
+++ src/infrastructure/interfaces/fetchData.interface.ts
@@ -2,5 +2,5 @@ import { NewsModel } from "../models/news.model";
 import { FetchQuery } from "../dto/fetch.query";
 
 export interface IFetcher {
-    fetchData(url: string, query?: FetchQuery): Promise<NewsModel[] | void>
+    fetchData(url: string, query?: FetchQuery): Promise<NewsModel[]>
 }
