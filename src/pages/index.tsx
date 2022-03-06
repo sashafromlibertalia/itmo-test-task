aa035681d4515d6e3fac34a96d29b03ecc00f3c6
Alexander <sashafromlibertalia@gmail.com>
2022-03-06 03:22:42 +0300

    test(fix): fixed tests

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,9 +3,6 @@ import Head from 'next/head'
 import styles from '../../styles/modules/Main.module.css'
 import Header from "../domain/header/Header";
 import CardGrid from "../domain/grid/CardGrid";
-import { Provider } from "react-redux";
-import { store } from "../infrastructure/store/store";
-
 
 const Home: NextPage = () => {
     return (
@@ -16,13 +13,11 @@ const Home: NextPage = () => {
                 <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
                 <meta name="description" content="Built with love by @sashafromlibertalia"/>
             </Head>
-            <Provider store={store}>
-                <Header/>
-                <div className={styles.container}>
-                    <h1 className={`${styles.title} ${styles.textColor}`}>Новости и события</h1>
-                    <CardGrid/>
-                </div>
-            </Provider>
+            <Header/>
+            <div className={styles.container}>
+                <h1 className={`${styles.title} ${styles.textColor}`}>Новости и события</h1>
+                <CardGrid/>
+            </div>
         </div>
     )
 }
--- tests/app.test.tsx
+++ tests/app.test.tsx
@@ -1,14 +1,22 @@
 import CardGrid from "../src/domain/grid/CardGrid";
 import { render, screen } from '@testing-library/react'
+import { Provider } from "react-redux";
+import { store } from "next/dist/build/output/store";
 
 describe('Test card grid', () => {
     test('Renders grid', () => {
-        const { container } = render(<CardGrid />)
+        const { container } = render(
+            <Provider store={store}>
+                <CardGrid />
+            </Provider>
+        )
         expect(container).toMatchSnapshot()
     })
 
     test('Rendered 9 cards', () => {
-        render(<CardGrid />)
+        render( <Provider store={store}>
+            <CardGrid />
+        </Provider>)
         const headings = screen.getAllByRole('heading')
         expect(headings.length).toBe(9)
     })
